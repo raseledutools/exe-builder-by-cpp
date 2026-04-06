1e95fbf14cdc003f149a92452bf977a6342947f2
raseledutools <bhmrasel60@gmail.com>
2026-04-06 07:45:11 +0600

    Update adblocker.cpp

--- adblocker.cpp
+++ adblocker.cpp
@@ -1,40 +1,39 @@
 #include <windows.h>
 #include <string>
 
+// MSVC লিংকারের জন্য রেজিস্ট্রি লাইব্রেরি
+#pragma comment(lib, "advapi32.lib")
+
 using namespace std;
 
 void ToggleAdBlock(bool enable) {
     HKEY hKey;
     
-    // Chrome Registry Path for Force Install
+    // Chrome Registry Path
     string chromePath = "SOFTWARE\\Policies\\Google\\Chrome\\ExtensionInstallForcelist";
-    // AdGuard Chrome Extension ID & Update URL
     string adGuardChrome = "bgnkhhnnamicmpeenaelnjfhikgbkllg;https://clients2.google.com/service/update2/crx";
     
-    // Edge Registry Path for Force Install
+    // Edge Registry Path
     string edgePath = "SOFTWARE\\Policies\\Microsoft\\Edge\\ExtensionInstallForcelist";
-    // AdGuard Edge Extension ID & Update URL
     string adGuardEdge = "pdffkfellgipmhklpdmokmckkkfcopbh;https://edge.microsoft.com/extensionwebstorebase/v1/crx";
 
     if (enable) {
-        // ক্রোম ব্রাউজারে AdGuard ফোর্স ইনস্টল করা
-        if (RegCreateKeyExA(HKEY_LOCAL_MACHINE, chromePath.c_str(), 0, NULL, 0, KEY_WRITE, NULL, &hKey, NULL) == ERROR_SUCCESS) {
-            RegSetValueExA(hKey, "1", 0, REG_SZ, (const BYTE*)adGuardChrome.c_str(), adGuardChrome.length() + 1);
+        if (RegCreateKeyExA(HKEY_CURRENT_USER, chromePath.c_str(), 0, NULL, 0, KEY_WRITE, NULL, &hKey, NULL) == ERROR_SUCCESS) {
+            // static_cast<DWORD> দিয়ে সাইজ কনভার্সন ওয়ার্নিং ফিক্স করা হয়েছে
+            RegSetValueExA(hKey, "1", 0, REG_SZ, (const BYTE*)adGuardChrome.c_str(), static_cast<DWORD>(adGuardChrome.length() + 1));
             RegCloseKey(hKey);
         }
         
-        // এজ ব্রাউজারে AdGuard ফোর্স ইনস্টল করা
-        if (RegCreateKeyExA(HKEY_LOCAL_MACHINE, edgePath.c_str(), 0, NULL, 0, KEY_WRITE, NULL, &hKey, NULL) == ERROR_SUCCESS) {
-            RegSetValueExA(hKey, "1", 0, REG_SZ, (const BYTE*)adGuardEdge.c_str(), adGuardEdge.length() + 1);
+        if (RegCreateKeyExA(HKEY_CURRENT_USER, edgePath.c_str(), 0, NULL, 0, KEY_WRITE, NULL, &hKey, NULL) == ERROR_SUCCESS) {
+            RegSetValueExA(hKey, "1", 0, REG_SZ, (const BYTE*)adGuardEdge.c_str(), static_cast<DWORD>(adGuardEdge.length() + 1));
             RegCloseKey(hKey);
         }
     } else {
-        // টিক তুলে নিলে AdGuard ডিলিট করে দেওয়া (Value Delete)
-        if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, chromePath.c_str(), 0, KEY_WRITE, &hKey) == ERROR_SUCCESS) {
+        if (RegOpenKeyExA(HKEY_CURRENT_USER, chromePath.c_str(), 0, KEY_WRITE, &hKey) == ERROR_SUCCESS) {
             RegDeleteValueA(hKey, "1");
             RegCloseKey(hKey);
         }
-        if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, edgePath.c_str(), 0, KEY_WRITE, &hKey) == ERROR_SUCCESS) {
+        if (RegOpenKeyExA(HKEY_CURRENT_USER, edgePath.c_str(), 0, KEY_WRITE, &hKey) == ERROR_SUCCESS) {
             RegDeleteValueA(hKey, "1");
             RegCloseKey(hKey);
         }
